1ad2df24efc45ee0eaf7587cd065ed21935622a8
Babuhatarii <ochegoharyon@gmail.com>
2024-02-01 04:36:40 -0800

    LEFT SUBTREE: CURRENT NODE: RIGHT SUBTREE:

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal.
+ * @tree: Pointer to the root node of the tree to traverse.
+ * @func: Pointer to a function to call for each node.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* Check if tree and func are not NULL */
+	if (tree != NULL && func != NULL)
+	{
+		/* Recursive call for the left subtree */
+		binary_tree_inorder(tree->left, func);
+		/* Call the function for the current node */
+		func(tree->n);
+		/* Recursive call for the right subtree */
+		binary_tree_inorder(tree->right, func);
+	}
+}
